edc1f22c259147dead42d6a6770e3e2e00e02ea9
Arshin99 <arshinkafi99@gmail.com>
2023-08-30 17:18:31 +0330

    Product styles added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Image from 'next/Image'
+import Link from 'next/Link'
 
 const Product = ({item}) => {
     return (
-      <div>
-        <div>{item.name}</div>
-        <div>Price : {item.price}</div>
-        <div>Size : {item.size}</div>
+      <div className="product-detail">
+        <div className="product-NameAndPrice">
+          <div>
+            <div className="product-Name">{item.name}</div>
+            <div>Price : {item.price}$</div>
+          </div>
+          <Link className="showMore-btn" href={`${item.category}/${item.id}`}>
+            Show More Detail
+          </Link>
+        </div>
         <Image
           src={item.image}
           alt="GFG logo served with static path of public directory"
-          height="100"
-          width="400"
+          height="20"
+          width="80"
         />
       </div>
     );
