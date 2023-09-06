f8fa3a379af0b74de953885a4677ee64ed9e89b6
Arshin99 <arshinkafi99@gmail.com>
2023-09-06 11:42:32 +0330

    search styles added

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,7 @@ import {useState,useCallback,useMemo} from 'react'
 import {searchByName} from '@/utils/searchByName'
 import Image from 'next/Image'
 import Link from 'next/Link'
+import SearchPhoto from "../assets/SearchPhoto.png";
 
 const Search = () => {
 
@@ -23,14 +24,18 @@ const Search = () => {
 
     return (
       <div className="search-container">
-        <label className="Link">Search :</label>
+        <label className="SearchTitle">Search :</label>
         <input value={search} onChange={handleChange} />
-
+        <Image src={SearchPhoto} height="24" width="24" />
         {!!search && (
           <div className="SearchResults">
             {RESULTS.map((product) => (
-              <Link href={`/${product.category}/${product.id}`} className='search-result' onClick={clickSearchResult}>
-                <Image src={product.image} height="7" width='28'/>
+              <Link
+                href={`/${product.category}/${product.id}`}
+                className="search-result"
+                onClick={clickSearchResult}
+              >
+                <Image src={product.image} height="7" width="28" />
                 <div>{product.name}</div>
               </Link>
             ))}
