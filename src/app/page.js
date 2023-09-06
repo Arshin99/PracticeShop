9c0de38d83782125e433c85b92d418bf59903ab9
Arshin99 <arshinkafi99@gmail.com>
2023-09-06 11:32:48 +0330

    Home buttons added

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,10 @@ export default function Home() {
       <div className="Home-Container">
         <h1 className="Home-Title">Online Shopping</h1>
         <p className="Home-desc">Find all your favorite supplies easily !</p>
+        <div className="homeBtns">
+          <button className="btn contact">Contact Us</button>
+          <button className="btn about">About Us</button>
+        </div>
       </div>
       <Image src={HomePage} width="700" height="500" className="Home-Image" />
     </div>
